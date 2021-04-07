e9f149b1c5a0b3b7ac8497660b4d8c4bdc04f33b
FirsovDmitriy <firsov.dmitriy666@gmail.com>
2021-04-07 12:46:05 +0400

    Updated block files

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,31 @@
 const burgerMenButton = document.querySelector('.menu__burger')
-console.log(burgerMenButton)
-
 const menuBody = document.querySelector('.menu__body')
 
 burgerMenButton.addEventListener('click', () => {
     burgerMenButton.classList.toggle('menu__burger--active')
     menuBody.classList.toggle('menu__body--active')
+})
+
+new Swiper('.rewiews__carousel', {
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next'
+    },
+
+    pagination: {
+        el: '.swiper-pagination'
+    },
+
+    // slidesPerView: 2,
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+
+        992: {
+            slidesPerView: 2
+        }
+    }
+   
 })
